ef43bdab361bfee55535aa06d8357bd47d1f1018
msemik <michal.semik@gmail.com>
2015-11-19 23:31:36 +0100

    Improved resolving jar filename

--- src/main/java/pl/uj/edu/engine/DefaultTaskReceiver.java
+++ src/main/java/pl/uj/edu/engine/DefaultTaskReceiver.java
@@ -1,5 +1,7 @@
 package pl.uj.edu.engine;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.ApplicationEventPublisher;
@@ -9,8 +11,7 @@ import pl.uj.edu.userlib.Task;
 import pl.uj.edu.userlib.TaskReceiver;
 
 import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.nio.file.Paths;
 
 @Configurable
 public class DefaultTaskReceiver implements TaskReceiver {
@@ -21,23 +22,23 @@ public class DefaultTaskReceiver implements TaskReceiver {
     @Autowired
     private TaskJarRegistry taskJarRegistry;
 
+    private Logger logger = LoggerFactory.getLogger(DefaultTaskReceiver.class);
+
     public void doAsync(Object task, Object callback) {
         Class taskClass = task.getClass();
         URL resource = taskClass.getResource('/' + taskClass.getName().replace('.', '/') + ".class");
 
         if (resource == null) {
+            logger.info("null resource occurred");
             return; // event, log
         }
 
-        Pattern jarFilenamePattern = Pattern.compile("([\\w-]+\\.jar!)"); // need better pattern for filename
-        Matcher matcher = jarFilenamePattern.matcher(resource.toString());
+        String[] resourcePartition = resource.toString().split("!");
+        if(resourcePartition == null || resourcePartition.length != 2)
+            throw new IllegalStateException("Unexpected jar resource format occurred:" + resource);
 
-        if (!matcher.find()) {
-            return; // event, log
-        }
-
-        String jarName = matcher.group().replace("!", "");
-
+        String jarName = Paths.get(resourcePartition[0]).getFileName().toString();
+        logger.info("Discovered jar filename from task:" + jarName);
         Task taskToDo = JclUtils.cast(task, Task.class);
         Callback callbackToDo = JclUtils.cast(callback, Callback.class);
 
