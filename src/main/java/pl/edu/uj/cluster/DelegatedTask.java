ed7eab19f26e0a5b34bcfea78fd047e59a3bfb95
alanhawrot <alan.hawrot@gmail.com>
2016-02-29 16:39:02 +0100

    Added Delegated and External Task Decorator classes

---
+++ src/main/java/pl/edu/uj/cluster/DelegatedTask.java
@@ -0,0 +1,50 @@
+package pl.edu.uj.cluster;
+
+import pl.edu.uj.engine.workerpool.WorkerPoolTask;
+
+import java.nio.file.Path;
+
+/**
+ * Created by alanhawrot on 29.02.2016.
+ */
+public class DelegatedTask implements WorkerPoolTask {
+    private WorkerPoolTask task;
+    private String destinationNodeId;
+
+    public DelegatedTask(WorkerPoolTask task, String destinationNodeId) {
+        this.task = task;
+        this.destinationNodeId = destinationNodeId;
+    }
+
+    public WorkerPoolTask getTask() {
+        return task;
+    }
+
+    public String getDestinationNodeId() {
+        return destinationNodeId;
+    }
+
+    public void setDestinationNodeId(String destinationNodeId) {
+        this.destinationNodeId = destinationNodeId;
+    }
+
+    @Override
+    public Path getJarName() {
+        return task.getJarName();
+    }
+
+    @Override
+    public int getTaskId() {
+        return task.getTaskId();
+    }
+
+    @Override
+    public boolean isExternal() {
+        return true;
+    }
+
+    @Override
+    public Object call() throws Exception {
+        return task.call();
+    }
+}
---
+++ src/main/java/pl/edu/uj/cluster/ExternalTask.java
@@ -0,0 +1,54 @@
+package pl.edu.uj.cluster;
+
+import pl.edu.uj.engine.workerpool.WorkerPoolTask;
+
+import java.nio.file.Path;
+
+/**
+ * Created by alanhawrot on 29.02.2016.
+ */
+public class ExternalTask implements WorkerPoolTask {
+    private WorkerPoolTask task;
+    private String sourceNodeId;
+
+    public ExternalTask(WorkerPoolTask task, String sourceNodeId) {
+        this.task = task;
+        this.sourceNodeId = sourceNodeId;
+    }
+
+    public WorkerPoolTask getTask() {
+        return task;
+    }
+
+    public void setTask(WorkerPoolTask task) {
+        this.task = task;
+    }
+
+    public String getSourceNodeId() {
+        return sourceNodeId;
+    }
+
+    public void setSourceNodeId(String sourceNodeId) {
+        this.sourceNodeId = sourceNodeId;
+    }
+
+    @Override
+    public Path getJarName() {
+        return task.getJarName();
+    }
+
+    @Override
+    public int getTaskId() {
+        return task.getTaskId();
+    }
+
+    @Override
+    public boolean isExternal() {
+        return true;
+    }
+
+    @Override
+    public Object call() throws Exception {
+        return task.call();
+    }
+}
