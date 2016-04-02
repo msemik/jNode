65c2bc513486b5d81999fa7d139aba7c337f75ca
msemik <michal.semik@gmail.com>
2016-04-02 16:04:38 +0200

    fix jgroups not big enough warnings on ubuntu

--- jNode.sh
+++ jNode.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 JAR_FILENAME="jnode-platform-0.0.1-SNAPSHOT.jar"
+OS=$(uname -s)
 
 if [  -f $CWD"/"$JAR_FILENAME ] ; then
     JNODE_JAR=$CWD"/"$JAR_FILENAME
@@ -15,7 +16,19 @@ if [ ! -f "$JNODE_JAR" ] ; then
     exit 1
 fi
 
-OS=$(uname -s)
+if [ "$OS" == "Linux" ]; then
+	rmem_max=`sysctl -b net.core.rmem_max`
+	if [ $rmem_max -lt 5242880 ]; then
+		echo "Need to extend maximum socket read buffer"
+		sudo sysctl -w net.core.rmem_max=5242880
+	fi
+
+	wmem_max=`sysctl -b net.core.wmem_max`
+	if [ $wmem_max -lt 5242880 ]; then
+		echo "Need to extend maximum socket write buffer"
+		sudo sysctl -w net.core.wmem_max=5242880
+	fi
+fi
 
 if [ "$OS" == "Darwin" ]; then
     java -jar -Djava.net.preferIPv4Stack=true $JNODE_JAR $@
